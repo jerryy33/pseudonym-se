cb49cbef1c01ee82ee05603c06cfb0dcf08ce7e8
Jeremy Herbst <jeremy.herbst@studium.uni-hamburg.de>
2022-11-15 19:06:47 +0100

    #5 added real pseudonym generation

--- src/vault/pseudonyms.py
+++ src/vault/pseudonyms.py
@@ -1,6 +1,23 @@
 """Module that produces truly random pseudonyms"""
+import secrets
+import string
 
 
 def generate_pseudonym(record: str) -> str:
     # TODO generate valid truly random pseudonym
-    return "pseudonym"
+    return generate_random_string(16)
+
+
+def generate_random_string(length: int) -> str:
+    """Generate a random string for a given length.
+    Contains digits and uppercase letters only
+
+    Args:
+        length (int): length of the generated random string
+
+    Returns:
+        str: random string
+    """
+    return "".join(
+        secrets.choice(string.ascii_uppercase + string.digits) for _ in range(length)
+    )
